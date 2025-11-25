aceb99727a1c941b0576295e609fad4a5e6148ff
MdMahdiHasanTazelly <mahdihasantazelly@gmail.com>
2025-11-25 20:33:26 +0600

    removed window reloader

--- src/pages/PostBody.js
+++ src/pages/PostBody.js
@@ -53,14 +53,6 @@ function PostBody() {
     }
 
 
-    // for reloading
-    useEffect(() => {
-        window.location.reload();
-    }, [posts, commentBody]);
-
-
-
-
     useEffect(() => {
 
         const token = localStorage.getItem('token');
